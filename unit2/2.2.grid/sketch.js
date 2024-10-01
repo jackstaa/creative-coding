c8f36ae76dcad5326b5c2c40adc68a1a38beb381
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-10-01 11:29:32 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -9,19 +9,19 @@ function draw() {
   translate(100,100);
   for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
-     const char = ["a","b","c","d","e","f"]
-      let hex = ""
-      for (let x = 0; x < 6, x++){
-        if (Math.floor(Math.random()*15 <= 9){
+     const char = ["a","b","c","d","e","f"] //create array of letters
+      let hex = "" // empty string
+      for (let x = 0; x < 6, x++){ // simple code to randomly generate a number between 0-14 i.e. hexadecimal
+        if (Math.floor(Math.random()*15 <= 9){ // if 0-9 use numbers 0-9
           hex = hex + Math.floor(Math.random()*10);
-        } else{
+        } else{ // if 10-14 use random char a-f
           hex = hex + random(char);
         }
       }
       push();
       translate(x * 100, y * 100);
       ellipse(0,0,100,100);
-      fill(hex);
+      fill(hex); // fill circle with hex code as color
       pop();
    }
 }
