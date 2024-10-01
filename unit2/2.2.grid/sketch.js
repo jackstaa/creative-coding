c33f8e1c5b1b7b59a2d099b89dff386acd52bb68
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-10-01 11:27:33 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -5,22 +5,23 @@ function setup() {
   // disable animation
   noLoop();
 }
-const char = ["a","b","c","d","e","f"]
-let hex = ""
-for (let x = 0; x < 6, x++){
-  if (Math.floor(Math.random()*15 <= 9){
-  hex = hex + Math.floor(Math.random()*10);
-  } else{
-    hex = hex + random(char);
-  }
-}
 function draw() {
   translate(100,100);
   for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
+     const char = ["a","b","c","d","e","f"]
+      let hex = ""
+      for (let x = 0; x < 6, x++){
+        if (Math.floor(Math.random()*15 <= 9){
+          hex = hex + Math.floor(Math.random()*10);
+        } else{
+          hex = hex + random(char);
+        }
+      }
       push();
       translate(x * 100, y * 100);
       ellipse(0,0,100,100);
+      fill(hex);
       pop();
    }
 }
