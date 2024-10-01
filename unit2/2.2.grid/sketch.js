ed5b80e018be152bad0d82ce12bc75453eb13147
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-10-01 11:17:41 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -7,5 +7,14 @@ function setup() {
 }
 
 function draw() {
+  translate(100,100);
+  for (let x = 0; x < 8; x++){
+   for (let y = 0; y < 8; y++){
+      push();
+      translate(x * 100, y * 100);
+      ellipse(0,0,100,100);
+      pop();
+   }
+}
   background(220);
 }
