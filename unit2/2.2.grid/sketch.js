66538d11045ed70516979dcc960b4034ea8dd281
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-10-01 12:03:15 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -8,9 +8,9 @@ function draw() {
   translate(100,100);
   for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
-     const char = ["a","b","c","d","e","f"] //create array of letters
-      let hex = "#" // empty string
-      for (let x = 0; x < 6, x++){ // simple code to randomly generate a number between 0-14 i.e. hexadecimal
+     const char = ["a","b","c","d","e","f"]; //create array of letters
+      let hex = "#"; // empty string
+      for (let x = 0; x < 6, x++){// simple code to randomly generate a number between 0-14 i.e. hexadecimal
         if (Math.floor(Math.random()*15 <= 9){ // if 0-9 use numbers 0-9
           hex = hex + Math.floor(Math.random()*10);
         } else{ // if 10-14 use random char a-f
