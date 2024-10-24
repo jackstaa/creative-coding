4ec5ade398779a2385baa70c892a7a5009088300
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-10-24 11:59:13 -0400

    Update sketch.js

--- unit3/3.1.bounce/sketch.js
+++ unit3/3.1.bounce/sketch.js
@@ -6,19 +6,19 @@ let bounce = false;
 
 function setup() {
   createCanvas(400, 400);
-  let xspeed = random(-4,4);
-  let yspeed = random(-4,4);
+  xspeed = random(-4,4);
+  yspeed = random(-4,4);
   circle(x, y, 50);
 }
 
 function draw(){
   background(220);
   // check for collision with left and right edge
-  if (x < 25 | x > 375){
+  if (x < 25 || x > 375){
       xspeed = -xspeed;// reverse the x direction
       bounce = true;
   }
-  if (y < 25 | y > 375){
+  if (y < 25 || y > 375){
       yspeed = -yspeed; // reverse the y direction
       bounce = true;
   }
