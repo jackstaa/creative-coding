d7b9a23e9edb7fde5dcad7567ee7667583a0e8a0
Zach Whalen <zach.whalen@gmail.com>
2024-10-29 11:30:01 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -1,11 +1,27 @@
 function setup() {
   // create a canvas
-  createCanvas(1200, 1200);
+  createCanvas(600, 600);
+
 
   // disable animation
   noLoop();
+
 }
 
 function draw() {
   background(220);
+
+  // for (let x = 80; x < 120; x += 5){
+
+  // 	circle(x,80,50);
+
+  // }
+   for (let x = 1; x < 9; x += 1){
+
+   	for (let y = 1; y < 9; y += 1){
+  	circle(x * 50,y * 50,50);
+  	}
+
+  }
+
 }
