791a04c58b34e3efefbed3281c2582768249ccb4
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-11-12 20:40:31 -0500

    Update sketch.js

--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -1,15 +1,15 @@
 let ripplers = [];
-
+//class constructor
 class Rippler{
   constructor(x,y){
     this.x = x,
     this.y = y,
-    this.ripple =[];//Store ripples diameter and opacity
+    this.ripple =[];//Store ripple diameter and opacity
   }
   addRipple(){ //adding new ripple
     this.ripple.push({diameter: 0, opacity: 255});
   }
-
+//function to draw a ripple and track its diameter and opacity
   drawRipples(){ // draw and update the ripples
     for(let i = 0; i< this.ripple.length; i++){
       let ripple = this.ripple[i];
@@ -23,11 +23,11 @@ class Rippler{
     this.ripple = this.ripple.filter(ripple=> ripple.opacity > 0); //remove ripples at opacity 0
   }
 }
-
+//function to create the canvas
 function setup() {
   createCanvas(800, 800);
 }
-
+//function to draw background and ripples
 function draw() {
   background("#152997");
   // Draw the ripples
@@ -35,12 +35,12 @@ function draw() {
     rippler.drawRipples();
   }
 }
+//function to capture mouse clicks 
+//10 ripples per click
 function mousePressed(){
   let newRipple = new Rippler(mouseX, mouseY);
   ripplers.push(newRipple);
-  //adding the amount of circles in the ripple
-  //timeout to add space between ripples
   for(let i = 0; i< 10; i++){
-    setTimeout(()=> newRipple.addRipple(), i *100);
+    setTimeout(()=> newRipple.addRipple(), i *100);   //timeouts on each ripple to add space between them
   }
 }
