d1e0d5f28cf244ae5b855663446ef80d02951951
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-11-12 19:40:12 -0500

    Update sketch.js

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,7 +1,79 @@
+let x = 0;
+let y = 0;
+let d = 0;
+let score = 0;
+let velocity = 3;
+let speedx = velocity;
+let speedy = velocity;
+let goalx = 0;
+let goaly = 0;
+let goalSize = 25;
+let img;
+
+//add a sprite
+function preload(){
+  img = loadImage("pac.png");
+  img2 = loadImage("map.png");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(934, 451); // same dims as the map
+  x = random(width);
+  y = random(height);
+  goalx = random(width);
+  goaly = random(height);
+
 }
 
 function draw() {
   background(220);
+  image(img2, 0,0, 934, 451 );
+  image(img, x-50,y-30, 90 ,60);
+  // distance formula
+  d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
+
+  // add movement
+  x += speedx;
+  y += speedy;
+  
+  //draw goal
+  rect(goalx, goaly, goalSize,goalSize);
+
+  if(mouseX > x){
+    // move to the right
+    speedx = velocity;
+  }else{
+    // move to the left 
+    speedx = -velocity;
+
+  }
+  if(mouseY > y){
+    // move to the right
+    speedy = velocity;
+  }else{
+    // move to the left 
+    speedy = -velocity;
+
+  }
+
+  // check for a collision with the ghost
+  if(d < 25){
+    score = score - 1;
+	velocity = velocity - 0.25 //make the ghost slower every time it catches the user
+    x = random(width);
+    y = random(height);
+  }
+
+  // check for a collision with the goal
+  if(mouseX > goalx & mouseX < goalx + goalSize & mouseY > goaly & mouseY < goaly+goalSize){
+    score = score + 1;
+	velocity = velocity + 0.25 //make the ghost faster after every goal
+    x = random(width);
+    y = random(height);
+
+    goalx = random(width);
+    goaly = random(height);
+  }
+  textSize(30);
+  text("score: "+ score, 50, 200);
 }
