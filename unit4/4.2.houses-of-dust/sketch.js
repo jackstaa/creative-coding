b96c477c3deb4fb57a4b2fe2456e0516b67ffbdd
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-11-14 12:07:37 -0500

    Update sketch.js

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,33 @@
+let poem = "";
+
+let grammar = tracery.createGrammar(
+  {
+  "material": [
+	"SAND",
+	"DUST",
+	"LEAVES",
+	"PAPER",
+	"TIN",
+	"ROOTS",
+	"BRICK",
+	"BROKEN DISHES",
+	"WOOD",
+	"STRAW",
+	"WEEDS"
+  ],
+  "origin": "A HOUSE OF #material#"
+}
+);
+
+poem = grammar.flatten("#origin#")
+
 function setup() {
   createCanvas(800, 800);
+  noLoop();
 }
 
 function draw() {
   background(220);
+
+  text(poem, 50, 50);
 }
