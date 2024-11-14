963c35ae15a7135c037684404c268e99c26f4ee5
Zach Whalen <zach.whalen@gmail.com>
2024-11-14 12:11:59 -0500

    demoing houses of dust

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,33 @@
+let quatrain = '';
+
+let grammar = tracery.createGrammar({
+  "material": [
+	"SAND",
+	"DUST",
+	"LEAVES",
+	"PAPER",
+	"TIN",
+	"ROOTS",
+	"BRICK",
+	"BROKEN DISHES",
+	"WOOD",
+	"STRAW",
+	"WEEDS"
+  ],
+  "origin": "A HOUSE OF #material#"
+});
+
+quatrain = grammar.flatten("#origin#");
+
+
 function setup() {
   createCanvas(800, 800);
+  noLoop();
 }
 
 function draw() {
   background(220);
+
+  textSize(40);
+  text(quatrain,50,70);
 }
