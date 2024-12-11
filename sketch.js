553fde05f1679e22bf11972f03dabb7daec52703
Jack C <84105527+jackstaa@users.noreply.github.com>
2024-12-11 09:49:45 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,8 +20,8 @@ function windowResized() {
 // function to draw the sky's gradient
 //pick 2 random colors
 function drawSkyGradient() {
-  let skyColorTop = color(random(150, 255), random(130), random(100)); // random, but warm color to radiate the sun's rays
-  let skyColorBottom = color(random(100), random(150, 200), random(200,255)); //random, but cool, trying to emulate the mountains behind the sea
+   let skyColorTop = color(random(180, 200), random(120), random(80)); // random, but warm color to radiate the sun's rays
+  let skyColorBottom = color(random(50), random(100, 140), random(160,210)); //random, but cool, trying to emulate the mountains behind the sea
   for (let y = 0; y < height; y++) { //draw the gradient
     let inter = map(y, 0, height, 0, 1);
     let col = lerpColor(skyColorTop, skyColorBottom, inter); // fade the effect so the sky doesnt stand out too much from the mountains
@@ -32,19 +32,19 @@ function drawSkyGradient() {
 
 //adding the ground and other stuff to this function in the changes
 function drawMountainLayers() {
-  let layers = 4;  // mountain layers
+  let layers = 6;  // mountain layers
   let layerHeight = 105;
 
   // function to draw the mountains
   // basically we want to choose a soft color but we want to have some variance as well
   // im going to take out the low end from each
   for (let i = 0; i < layers; i++) {
-    let yOffset = height - 200-  i * layerHeight;
+    let yOffset = height - 100-  i * layerHeight;
     let mountainColor = color(
-      random(100, 200), // Red
+      random(100, 130), // Red
       random(5, 50), // Green
-      random(125, 225), // Blue
-      120              // Alpha
+      random(105, 155), // Blue
+      90              // Alpha
     );
     fill(mountainColor);
     noStroke();
@@ -63,14 +63,14 @@ function drawMountainLayers() {
 function drawSun() {
   // randomize sun position within the top of the canvas
   let sunX = random(100, width - 100);
-  let sunY = random(50, height * 0.25);
+  let sunY = random(50, height * 0.15);
   
   // sun layers for a cooler sun
   let sunColors = [
-    color(255, 255, 0, 50),   // increase alpha
-    color(255, 255, 0, 100),  // between indexes
-    color(255, 255, 0, 150),  // of colors array
-    color(255, 255, 0, 200)   // to make the sun look better
+    color(245, 141, 86, 50),   // increase alpha
+    color(245, 141, 86, 100),  // between indexes
+    color(245, 141, 86, 150),  // of colors array
+    color(245, 141, 86, 200)   // to make the sun look better
   ];
     // draw the sun with the array of sunColors
   for (let i = sunColors.length - 1; i >= 0; i--) {
